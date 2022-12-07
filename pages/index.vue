1adb0a2733c7e388b19817d87e11d7cdc7f46cbe
Kudakwashe Mukombachoto <42964245+Kudakwashe24@users.noreply.github.com>
2022-12-07 14:49:12 +0200

    project_files

---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+    <h1>My TODO list for the day</h1>
+    <p>I will add my todo items first thing every morning and I will delete it or click done once
+    I've done that task.</p>
+
+    <div class="create-new">
+      <input type="text"
+      v-model="newTask"
+      placeholder="Add a new task"
+      @keypress.enter="addTask" />
+
+      <button @click="addTask">Add</button>
+    </div>
+
+    <div class="tasks">
+      <Task
+        v-for="(task, i) in $store.state.tasks"
+        :key="i"
+        :task="task" />
+    </div>
+
+
+  </main>
+</template>
+
+<script>
+export default {
+  data (){
+    return {
+    newTask: ''
+    }
+  },
+  methods: {
+    addTask () {
+      if (this.newTask)
+        this.$store.commit('ADD_TASK', this.newTask)
+        this.newTask = '';
+      }
+    }
+}
+</script>
